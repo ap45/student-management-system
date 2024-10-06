aff39c8f2c66581a2a9c260a50ec0f3c72de2e0c
ap45 <arpit.parekh0104@gmail.com>
2024-10-06 18:39:57 -0400

    removed the secret

--- main_app/hod_views.py
+++ main_app/hod_views.py
@@ -600,7 +600,7 @@ def send_student_notification(request):
             'to': student.admin.fcm_token
         }
         headers = {'Authorization':
-                   'key=AAAA3Bm8j_M:APA91bElZlOLetwV696SoEtgzpJr2qbxBfxVBfDWFiopBWzfCfzQp2nRyC7_A2mlukZEHV4g1AmyC6P_HonvSkY2YyliKt5tT3fe_1lrKod2Daigzhb2xnYQMxUWjCAIQcUexAMPZePB',
+                   'key=Your-Key',
                    'Content-Type': 'application/json'}
         data = requests.post(url, data=json.dumps(body), headers=headers)
         notification = NotificationStudent(student=student, message=message)
